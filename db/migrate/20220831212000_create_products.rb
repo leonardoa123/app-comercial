f65ed057e360398c9d6fe7955be868d2fb864590
Leonardo <leonardoa123@gmail.com>
2022-08-31 17:24:03 -0400

    agregar modelos 2

--- db/migrate/20220831212000_create_products.rb
+++ db/migrate/20220831212000_create_products.rb
@@ -1,6 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
+      t.string :imagen
       t.string :nombre
       t.string :descripcion
       t.integer :existencia
